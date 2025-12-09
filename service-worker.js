c3373386cc579860698eb3c05580a93f588b6f03
aikuznetsov7-beep <aikuznetsov7@gmail.com>
2025-12-09 22:01:02 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,13 @@
-self.addEventListener("install",e=>{e.waitUntil(caches.open("v1").then(c=>c.addAll(["index.html","manifest.json","icon192.png","icon512.png"])))}); 
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+
+self.addEventListener('install', e=>{
+ e.waitUntil(
+   caches.open('v1').then(cache=>cache.addAll([
+     'index.html','data.json','manifest.json','icon192.png','icon512.png'
+   ]))
+ );
+});
+self.addEventListener('fetch', e=>{
+ e.respondWith(
+   caches.match(e.request).then(resp=>resp || fetch(e.request))
+ );
+});
